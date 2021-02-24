53fb6146f12b4766c87ee3bc611356813e699553
Elijah Samuels <elijahsamuels@gmail.com>
2021-02-23 17:20:48 -0700

    commit tree 6369f71143c703e81d172a174b80c6f07e03632b

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Add your code here
+
+
+// fetch(`http://localhost:3000/users`, {
+// 	method: "POST",
+// 	headers: {
+// 	  "Content-Type": "application/json",
+// 	  "Accept": "application/json",
+// 	}
+//   });
+
+function submitData( name, email ) {
+	return fetch( 'http://localhost:3000/users', {
+		method: "POST",
+		headers: {
+		  "Content-Type": "application/json",
+		  "Accept": "application/json"
+		},
+		body: JSON.stringify( {
+		  name,
+		  email
+		} )
+	  } )
+	  
+	   	.then(function(response) {
+	  		return response.json();
+	  	})
+		.then(function(object) {
+			document.body.innerHTML = object.id
+		
+	  	})
+	  	.catch(function(error) {
+	  		// alert("Bad things! Ragnarők!");
+			  document.body.innerHTML = error.message;
+	  	});
+}
+  
+
+// function submitData(name, email) {
+	
+// 	let configObj = {
+// 		headers: {
+// 			"Content-Type": "application/json",
+// 			"Accept": "application/json"
+// 		},
+// 		body: JSON.stringify({name,email})
+// 	};
+
+// 	return fetch("http://localhost:3000/users", configObj)
+
+
+
+
+	
+	
+	
+// } 
